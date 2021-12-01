e2364c43c07d78bea1672050ceb200254bce7876
klesio Nascimento <klesio.dev@gmail.com>
2021-12-01 08:24:29 -0300

    change in pages

---
+++ pageAuthLogin/js/onmouseover-register.js
@@ -0,0 +1,12 @@
+/* let button = document.getElementById('register');
+
+let zoom = 2
+
+button.onmousemove = function () {
+  this.style.width = (this.clientHeight * zoom) + 'px';
+
+}
+
+button.onmouseout = function () {
+  this.style.width = (this.clientHeight / zoom) + 'px'
+} */
