7bb5b0870eebcee2530ff3a0c0205e26c374e6d1
klesio Nascimento <klesio.dev@gmail.com>
2021-12-03 18:34:19 -0300

    toggle between login and login

--- pageAuthLogin/js/change-auth-login.js
+++ pageAuthLogin/js/change-auth-login.js
@@ -1,27 +1,60 @@
 const enter = document.querySelector('.to-enter')
 const register = document.querySelector('.to-register')
 
+const selectorEnter = document.querySelector('.selector-enter')
+const selectorLogin = document.querySelector('.selector-login')
+
+function selector(){
+  selectorEnter.classList.toggle('touch-ripple-root')
+  selectorLogin.classList.toggle('touch-ripple-root')
+}
 
 function auth() {
-  let auth = document.querySelector('.auth');
-  auth.classList.toggle('block');
+  const auth = document.querySelector('.auth');
+  const login = document.querySelector('.login');
 
-  enter.disabled = true;
-  register.disabled = false;
+  let contains = auth.classList.contains('display-none');
+
+  if (contains) {
+    auth.classList.remove('display-none')
+    auth.classList.add('display-inital')
+
+
+    login.classList.remove('display-inital')
+    login.classList.add('display-none')
+
+    enter.disabled = true;
+    register.disabled = false;
+
+    selector()
+
+  }
+  
 }
-
-
 
 function login() {
-  let login = document.querySelector('.login');
-  login.classList.toggle('block');
 
+  const auth = document.querySelector('.auth');
+  const login = document.querySelector('.login');
 
-  enter.disabled = false;
-  register.disabled = true;
+  const contains = login.classList.contains('display-none');
+
+  if (contains) {
+    login.classList.remove('display-none')
+    login.classList.add('display-inital')
+
+    auth.classList.remove('display-inital')
+    auth.classList.add('display-none')
+
+    enter.disabled = false;
+    register.disabled = true;
+    
+    selector()
+    
+  }
+
 
 }
-
 
 enter.addEventListener('click', auth)
 register.addEventListener('click', login)
